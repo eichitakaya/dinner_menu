4bf4c26b931e3892cc6a72917bbe8d4d2624caf2
eichitakaya <takaya.wisdom0616@gmail.com>
2024-08-19 14:28:45 +0900

    added ingredients and images

--- app.py
+++ app.py
@@ -13,10 +13,14 @@ LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
 LINE_USER_ID = os.getenv('LINE_USER_ID')
 APP_PASSWORD = os.getenv('APP_PASSWORD')
 
+menu_data = {}
+
 def load_menu():
+    global menu_data
     df = pd.read_csv('menu.csv')
+    menu_data = df.set_index("item").to_dict("index")
     categories = df.groupby("category")["item"].apply(list).to_dict()
-    return categories
+    return categories, menu_data
 
 @app.route('/')
 def password():
@@ -35,21 +39,22 @@ def check_password():
 def index():
     if not session.get('authenticated'):
         return redirect(url_for('password'))
-    categories = load_menu()
-    return render_template('index.html', categories=categories)
+    categories, menu_data = load_menu()
+    return render_template('index.html', categories=categories, menu_data=menu_data)
 
 @app.route('/send-notification', methods=['POST'])
 def send_notification():
     data = request.get_json()
     #print(LINE_CHANNEL_ACCESS_TOKEN)
     menu_item = data.get('menuItem')
+    ingredient = menu_data[menu_item]['ingredient']
     print(menu_item)
     url = 'https://notify-api.line.me/api/notify'
     headers = {
         'Authorization': f'Bearer {LINE_CHANNEL_ACCESS_TOKEN}'
     }
     data = {
-        'message': f'{menu_item}が選ばれました。'
+        'message': f'{menu_item}が選ばれました。\n材料: {ingredient}'
     }
     response = requests.post(url, headers=headers, data=data)
     return jsonify(response.json())
